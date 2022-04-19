f2844e1813d157f5822f1338f094c3339a293df6
Filippo Orru <filippoorru@hotmail.com>
2022-04-19 08:58:04 +0200

    Deploy script

---
+++ main.py
@@ -0,0 +1,23 @@
+import subprocess
+from github_webhook import Webhook
+from flask import Flask
+
+secret = open("secret", "r").readline()
+
+app = Flask(__name__)
+webhook = Webhook(app, secret=secret)  # Defines '/postreceive' endpoint
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+
+@webhook.hook()  # Defines a handler for the 'push' event
+def on_push(data):
+    subprocess.run(["sh", "deploy.sh"])
+    return "OK"
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=40148)
